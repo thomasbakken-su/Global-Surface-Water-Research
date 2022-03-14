e761002145ec575791a40200940aef08c8f62543
thomasbakken-su <tbakken@seattleu.edu>
2022-03-13 17:41:18 -0700

    Histogram update
    added code for

--- downloadWaterData.py
+++ downloadWaterData.py
@@ -1,12 +1,16 @@
 import urllib.request, sys, getopt, os
 
-class downloadWaterData:
-    def __init__(self, destination_folder, dataset_name):
-        self.DESTINATION_FOLDER = destination_folder
-        self.DATASET_NAME = dataset_name
-        self.processDest()
-        self.prepareLatsNLongs()
+OCCURENCE = 'occurence'
+CHANGE = 'change'
+SEASONALITY = 'seasonality'
+RECURRANCE = 'recurrence' 
+TRANSITIONS = 'transitions'
 
+
+class downloadWaterData:
+    def __init__(self, destination_folder):
+        self.DESTINATION_FOLDER = destination_folder
+        self.processDest()
 
     def processDest(self):
         if (self.DESTINATION_FOLDER[-1:]!="/"):
@@ -22,21 +26,19 @@ class downloadWaterData:
         lats.extend([str(n) + "N" for n in range(0,90,10)])
         return lats, longs
 
-    def downloadAllData(self):
-        lats, longs = self.prepareLatsNLongs()
-        self.downloadData(lats, longs)
-
-    def downloadData(self, lats, longs):
+    def downloadData(self, dataset, lats = None, longs = None):
+        if not lats or not longs:
+            lats, longs = self.prepareLatsNLongs()
         fileCount = len(longs)*len(lats)
         counter = 1
         for lng in longs:
             for lat in lats:
                 print(lat, lng)
-                filename = self.DATASET_NAME+ "_" + str(lng) + "_" + str(lat) + "v1_3_2020.tif"
+                filename = dataset + "_" + str(lng) + "_" + str(lat) + "v1_3_2020.tif"
                 if os.path.exists(self.DESTINATION_FOLDER + filename):
                     print(self.DESTINATION_FOLDER + filename + " already exists - skipping")
                 else:
-                    url = "http://storage.googleapis.com/global-surface-water/downloads2020/" + self.DATASET_NAME + "/" + filename
+                    url = "http://storage.googleapis.com/global-surface-water/downloads2020/" + dataset + "/" + filename
                     code = urllib.request.urlopen(url).getcode()
                     if (code != 404):
                         print("Downloading " + url + " (" + str(counter) + "/" + str(fileCount) + ")")
